3d9f31ea22234a0f9ea7852ae4e58992542e655d
Sangram Reddy <reddy.horcrux@gmail.com>
2019-11-19 15:56:43 +0000

    Initial Commit

---
+++ Customer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Customer
+//
+//  Created by Sangram Reddy on 19/11/2019.
+//  Copyright © 2019 Sangram Reddy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
